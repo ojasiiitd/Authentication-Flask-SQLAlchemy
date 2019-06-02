517c13483071e6819f04ce4579b9867b65ff4398
ojasiiitd <ojassaxena03@gmail.com>
2019-06-03 00:23:47 +0530

    Importing forms from forms.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABSE_URI"] = "sqlite:///info.db"
 db = SQLAlchemy(app)
+pwd = Bcrypt(app)
 
 from app import routes
