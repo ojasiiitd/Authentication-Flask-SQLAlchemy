68859d370337b5fb6a39c1b99d246ed705822290
ojasiiitd <ojassaxena03@gmail.com>
2019-06-03 00:26:55 +0530

    Removing unimportant elements

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class User(db.Model) :
     time = db.Column(db.DateTime , default = datetime.now)
     uname = db.Column(db.String(20) , unique = True , nullable = False)
     email = db.Column(db.String(50) , unique = True , nullable = False)
-    password = db.Column(db.String(100) , nullable = False)
+    password = db.Column(db.String(60) , nullable = False)
 
     def __repr__(self):
         return  'User(%s , %s)' % (self.uname , self.email)
