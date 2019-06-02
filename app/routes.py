a5538cb091c28715e7d6a3869d361a53a5759cf4
ojasiiitd <ojassaxena03@gmail.com>
2019-06-03 00:25:25 +0530

    Added forms

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,20 @@
 from flask import render_template , flash , redirect , url_for , request
 from app import app
+from app.forms import RegForm , LoginForm
 from app.models import User
 
 @app.route('/')
 def homepage():
     return render_template("information.html")
 
-@app.route("/login")
-def loginpage():
-    return render_template("information.html")
-
-@app.route("/signup")
+@app.route("/signup" , methods = ['GET' , 'POST'])
 def signuppage() :
-    return render_template("information.html")
+    form = RegForm()
+    if request.method == "POST" and form.validate_on_submit() :
+        flash('Account created for %s !' % (form.uname.data))
+    return render_template("signup.html" , form = form)
+
+@app.route("/login" , methods = ['GET' , 'POST'])
+def loginpage():
+    form = LoginForm()
+    return render_template("login.html" , form = form)
