f95d8dd4e94b4a2607c80076393f8201171a2e0c
ojasiiitd <ojassaxena03@gmail.com>
2019-06-04 14:12:44 +0530

    Putting elements in database

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template , flash , redirect , url_for , request
 from app import app
 from app.forms import RegForm , LoginForm
 from app.models import User
+from app import app, db, pwd
 
 @app.route('/')
 def homepage():
@@ -10,13 +11,13 @@ def homepage():
 @app.route("/signup" , methods = ['GET' , 'POST'])
 def signuppage() :
     form = RegForm(request.form)
-    if request.method == "POST" :
-        if form.validate() :
-            flash("Account created for %s!" % (form.uname.data) , "success")
-            return redirect(url_for("homepage"))
-        # else :
-        #     flash("Sorry, account could not be created at this time" , "danger")
-        #     return redirect(url_for("signuppage"))
+    if request.method == "POST" and form.validate():
+        hashed = pwd.generate_password_hash(form.password.data).decode('utf-8')
+        element = User(uname = form.uname.data , email = form.email.data , password = hashed)
+        db.session.add(element)
+        db.session.commit()
+        flash("Account created for %s!" % (form.uname.data) , "success")
+        return redirect(url_for("loginpage"))
     else :
         return render_template("signup.html" , form = form)
 
@@ -27,7 +28,7 @@ def loginpage():
         if form.validate() :
             flash("Welcome, %s!" % (form.uname.data) , "success")
             return redirect(url_for("homepage"))
-        # else :
-        #     flash("Sorry, account could not be created at this time" , "danger")
-        #     return redirect(url_for("signuppage"))
+        else :
+            flash("Username or Password doesn't match, please try again." , "danger")
+            return redirect(url_for("loginpage"))
     return render_template("login.html" , form = form)
