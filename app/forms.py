2b2dd6f4210c6bc5b9aab6e00cabb166a5597f8c
ojasiiitd <ojassaxena03@gmail.com>
2019-06-04 14:10:42 +0530

    Adding a check for duplicate username and email

--- app/forms.py
+++ app/forms.py
@@ -1,33 +1,43 @@
-from wtforms import Form , StringField , PasswordField , SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import Form , StringField , PasswordField , SubmitField , validators
+from app.models import User
 
 class RegForm(Form) :
     uname = StringField("Username" , validators=[
-        DataRequired() ,
-        Length(min=4)
+        validators.DataRequired() ,
+        validators.Length(min=4)
     ])
     email = StringField("E-Mail" , validators=[
-        DataRequired() ,
-        Email() ,
-        Length(min=7)
+        validators.DataRequired() ,
+        validators.Email() ,
+        validators.Length(min=6)
     ])
     password = PasswordField("Password" , validators=[
-        DataRequired() ,
-        Length(min=4 , max=20)
+        validators.DataRequired() ,
+        validators.Length(min=4 , max=20)
     ])
     confirm = PasswordField("Confirm Password" , validators=[
-        EqualTo('password') ,
-        DataRequired()
+        validators.EqualTo('password') ,
+        validators.DataRequired()
     ])
     submit = SubmitField("Sign Up")
 
+    def validate_uname(self , uname) :
+        present = User.query.filter_by(uname = uname.data).first()
+        if present:
+            raise validators.ValidationError("This username has already been taken, please choose a different one.")
+    
+    def validate_email(self , email) :
+        present = User.query.filter_by(email = email.data).first()
+        if present:
+            raise validators.ValidationError("This email has already been registered with us, please enter a different one.")
+
 class LoginForm(Form) :
     uname = StringField("Username" , validators=[
-        DataRequired() ,
-        Length(min=4 , max=20)
+        validators.DataRequired() ,
+        validators.Length(min=4 , max=20)
     ])
     password = PasswordField("Password" , validators=[
-        DataRequired() ,
-        Length(min=4 , max=20)
+        validators.DataRequired() ,
+        validators.Length(min=4 , max=20)
     ])
     submit = SubmitField("Login")
