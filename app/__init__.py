aaa3d9b94387a7bec69600d883348e40f677e91c
ojasiiitd <ojassaxena03@gmail.com>
2019-06-04 21:23:30 +0530

    Login manager info added

--- app/__init__.py
+++ app/__init__.py
@@ -8,5 +8,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 pwd = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "loginpage"
+login_manager.login_message = "You are mot authorised to access this page. Please login/signup first."
+login_manager.login_message_category = "danger" 
 
 from app import routes
