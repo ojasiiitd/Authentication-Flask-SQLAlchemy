382d8d6adbec8ed76dda23eb9b291510ac0cc16a
ojasiiitd <ojassaxena03@gmail.com>
2019-06-04 14:04:23 +0530

    Initializing Databse

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABSE_URI"] = "sqlite:///info.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+db.init_app(app)
 pwd = Bcrypt(app)
 
 from app import routes
