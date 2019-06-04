c053404587ecbf9fd6a1bde8eb029f54c5ca176c
ojasiiitd <ojassaxena03@gmail.com>
2019-06-04 19:58:01 +0530

    Adding login_manager for logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-db.init_app(app)
 pwd = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from app import routes
