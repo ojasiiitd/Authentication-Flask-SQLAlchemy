6da5807fe29d3fd48b2d7f6d39e50f3b84fae9fc
ojasiiitd <ojassaxena03@gmail.com>
2019-06-04 21:24:17 +0530

    Adding custom get_id function (sno is id here)

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    return User.query.get(int(user_id))
 
 class User(db.Model , UserMixin) :
     sno = db.Column(db.Integer , primary_key = True)
@@ -12,6 +12,12 @@ class User(db.Model , UserMixin) :
     uname = db.Column(db.String(20) , unique = True , nullable = False)
     email = db.Column(db.String(50) , unique = True , nullable = False)
     password = db.Column(db.String(60) , nullable = False)
+
+    def get_id(self):
+        try:
+            return (self.sno)
+        except AttributeError:
+            raise NotImplementedError('No `id` attribute - override `get_id`')
 
     def __repr__(self):
         return  'User(%s , %s)' % (self.uname , self.email)
